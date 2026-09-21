9a73a2f7c73ad293290b55755603065b645f60d3
Arturo Abella <abellaarturo2@gmail.com>
2026-09-21 13:08:19 -0500

    Revert "se realizo la implementacion de los JPA,V2 y V3 mas correccioln de V1"
    This reverts commit cedf64e4579d9e7422dc57487b632c56f64d1b3d.

--- src/main/resources/db/migration/V3__add_streaming_url_to_event.sql
+++
@@ -1,7 +0,0 @@
--- =============================================================================
--- V3__add_streaming_url_to_event.sql
--- Agrega streaming_url VARCHAR(500) nullable a events sin modificar V1
--- =============================================================================
-
-ALTER TABLE events
-    ADD COLUMN streaming_url VARCHAR(500);
